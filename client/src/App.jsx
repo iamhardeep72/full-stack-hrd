75b2c5d57c25d69d504991fd89d95ff00612f973
matharu7 <matharu7777@gmail.com>
2026-04-21 16:02:19 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const fetchTodos = async () => {
+    const res = await axios.get("http://localhost:5000/api/todos");
+    setTodos(res.data);
+  };
+
+  const addTodo = async () => {
+    await axios.post("http://localhost:5000/api/todos", { text });
+    setText("");
+    fetchTodos();
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Todo App</h1>
+
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter todo"
+      />
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map((t) => (
+          <li key={t.id}>{t.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
